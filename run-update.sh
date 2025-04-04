5fc5edb6a7d2c5b7a60c8b976e13c9d66db8687b
marvin1099 <marvin.d1099@gmail.com>
2025-04-04 23:27:15 +0200

    add conf generation with topgrade

--- run-update.sh
+++ run-update.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 
-uptoml="$HOME/.config/topgrade.toml"
 logfile="/tmp/topgrade-report.log"
+uptoml="$HOME/.config/topgrade.toml"
 
 sudo touch "$logfile"
 sudo chmod 777 "$logfile" 2>/dev/null
+sudo chown root:root "$logfile"
 
+g=$(timeout 1 topgrade --edit-config > /dev/null 2>&1)
+sudo mkdir -p "$HOME/.config"
+sudo chown "$USER":"$USER" "$HOME/.config"
+sudo chmod u+rwx "$HOME/.config"
 sudo touch "$uptoml"
+sudo chown "$USER":"$USER" "$uptoml"
+sudo chmod u+rwx "$uptoml"
+
 misc=0
-if sudo cat "$uptoml" | grep "[misc]" > /dev/null; then
+if cat "$uptoml" | grep "[misc]" > /dev/null; then
     misc=1
 fi
-if sudo cat "$uptoml" | grep "assume_yes =" > /dev/null; then
+if cat "$uptoml" | grep "assume_yes =" > /dev/null; then
     if [[ $misc -eq 1 ]]
     then
-        sudo sed -i '/assume_yes =/c\assume_yes = true' "$uptoml" > /dev/null
+        sed -i '/assume_yes =/c\assume_yes = true' "$uptoml" > /dev/null
     else
-        sudo sed -i '/assume_yes =/c\[misc]\nassume_yes = true' "$uptoml" > /dev/null
+        sed -i '/assume_yes =/c\[misc]\nassume_yes = true' "$uptoml" > /dev/null
     fi
 else
     if [[ $misc -eq 1 ]]
     then
-        sudo sed -i '/\[misc\]/c\[misc]\nassume_yes = true' "$uptoml" > /dev/null
+        sed -i '/\[misc\]/c\[misc]\nassume_yes = true' "$uptoml" > /dev/null
     else
-        sudo echo "[misc]"$'\n'"assume_yes = true" >> "$uptoml"
+        echo "[misc]"$'\n'"assume_yes = true" >> "$uptoml"
     fi
 fi
 
