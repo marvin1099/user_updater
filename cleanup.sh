61f34e62e440a839996e2827132c5cbe14e65c75
marvin1099 <marvin1099@noreply.codeberg.org>
2025-07-14 01:33:39 +0200

    Add cleanup file

---
+++ cleanup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Ensure the script is run as root
+if [ "$EUID" -ne 0 ]; then
+    echo "This script must be run as root. Exiting."
+    exit 1
+fi
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+cd "$SCRIPTPATH" || exit 1
+
+loginfo=$(./main_logger.sh "" "Cleanup" "Update" "update")
+UUPDATER_IDATE=$(echo "$loginfo" | sed -n '1p')
+export UUPDATER_IDATE
+UUPDATER_ACTION=$(echo "$loginfo" | sed -n '2p')
+export UUPDATER_ACTION
+admin_log=$(echo "$loginfo" | sed -n '3p')
+log() {
+    echo "$1" | tee -a "$admin_log"
+}
+echo "$loginfo" | sed -n '4,$p'
+
+./delete_and_note_users.sh
+
+log "Cleaning report file"
+rm -f /tmp/topgrade-report.log
