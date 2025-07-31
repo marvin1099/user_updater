5b72aef8eae74f0dd12f118de33657696ecf235d
marvin1099 <marvin1099@noreply.codeberg.org>
2025-07-31 10:39:34 +0200

    Fix Distro var not printed correctly

--- run_update.sh
+++ run_update.sh
@@ -147,7 +147,7 @@ run_with_watchdog() {
                     fi
                     ms=$'The update won\'t seem to finish. START A MANUAL UPDATE.'
                     ms+=$'\nYou can update manually by running your Linux distro\'s update command.'
-                    ms+=$'\nTry websearching: "How to update on $Distro"'
+                    ms+=$'\nTry websearching: "How to update on'"$Distro"'"'
                     ms+=$'\nIf you\'re unsure, ask your system administrator for help.'
                     ms+=$'\nClose this window with the command: sudo /var/lib/user_updater/cleanup.sh &'
                 else
